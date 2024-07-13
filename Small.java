3816b567eb24ef31c6ed2f8735d6c009e2cf9c63
ARKThrishala <160831720+ARKThrishala@users.noreply.github.com>
2024-07-13 12:30:51 +0530

    Add files via upload

---
+++ Small.java
@@ -0,0 +1,26 @@
+class Small
+{
+	public static void main(String[] args0
+			{
+				int a=1;
+				int b=2;
+				int c=3;
+				if(a<b){
+					System.out.println("smallest is"+a);
+				}
+				else{
+					System.out.println("smallest is"+c);
+				}
+			
+		}
+		else{
+			if(b>c){
+				System.out.println("smallest is"+b);
+			}
+			else{
+				System.out.println("smallest is"+a);
+			}
+		}
+}
+
+}
